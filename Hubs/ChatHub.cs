7ded38fdcb1bd9b436f20c400be4b7356f24390f
Sallal Haider <87648696+Sidhugee@users.noreply.github.com>
2022-07-12 10:18:56 +0100

    Add files via upload

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorChatSignalR.Server.Hubs
+{
+    // SignalR Hub is basicaaly used for maintaining the connection btw the server and clients. 
+    public class ChatHub: Hub
+    {
+       
+// This function is basically main hub that initialize the communicating way btw been server and clients
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage" , user, message); 
+        }
+    }
+}
